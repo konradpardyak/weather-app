720ec2371438c5ad5486bf0de938bed5a3053c13
Konrad Pardyak <konradpardyak@gmail.com>
2021-03-17 12:38:08 +0100

    Add axios and API request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Content from './Content';
 
 const App = () => {
+  const [cityName, setCityName] = useState("London");
+  const [cityInfo, setCityInfo] = useState();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    const API_KEY = "8474678f08288c9925e4023da4548128";
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${API_KEY}`).then((res) => {
+      const responseCityInfo = res.data;
+      setCityInfo(responseCityInfo.city);
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${res.data.city.coord.lat}&lon=${res.data.city.coord.lon}&appid=${API_KEY}`).then((res) =>{
+        const responseWeather = res.data;
+        setWeather(responseWeather);
+      }).catch(error => console.log(error.response));
+    }).catch(error => console.log(error.response));
+  }, [cityName]);
+  
   return (
     <div className="App">
-      Weather App
+      {
+        weather ? <Content cityInfo={cityInfo} weather={weather} /> : <div>Loading page</div>
+      }
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,9 @@
+const Content = (props) => {
+  const {cityInfo, weather} = props;
+
+  return(
+    <div>Weather App Content</div>
+  )
+}
+
+export default Content;
